5e2afc93d2c7426ecaa0590dd1f4d37244b33b2c
maple-leaf10 <75773766+maple-leaf10@users.noreply.github.com>
2021-10-06 10:02:43 +0800

    更新 pg6 页面设置
    调整 pg6 的页面设置，使得其更符合联机设置

--- js/pg6.js
+++ js/pg6.js
@@ -1,73 +1,54 @@
 var btn1 = document.getElementById('btn1');
-var pic1 = document.querySelectorAll('#pic1');
-var pic2 = document.querySelectorAll('#pic2');
-var pic3 = document.querySelectorAll('#pic3');
-var pic4 = document.querySelectorAll('#pic4');
-var pic5 = document.querySelectorAll('#pic5');
-var pic6 = document.querySelectorAll('#pic6');
+var pic1 = document.getElementById('pic1');
+var pic2 = document.getElementById('pic2');
+var pic3 = document.getElementById('pic3');
+var pic4 = document.getElementById('pic4');
+var pic5 = document.getElementById('pic5');
+var pic6 = document.getElementById('pic6');
 var result_player1 = document.querySelector('.output_player1');
-var result_player2 = document.querySelector('.output_player2');
-var result = null;
 btn1.onclick = function() {
-    window.open('pg1.html');
+    window.location.href = 'pg1.html';
 }
-var j = 0;
-var flag = new Array(2);
-flag[0] = 0;
-flag[1] = 0;
-var flag2 = 0;
-var flag3 = 0;
-var start = document.querySelectorAll('#start');
-var stop1 = document.querySelectorAll('#stop');
-start[0].onclick = function() {
-    j = 0;
-    if (flag[0] == 1) {
+setTimeout("alert('玩家已进入联机页面，即将开始连接')",0);
+function blink() {
+    var color="#FF00FF,#FF00CC,#FF0099,#FF0066,#FF0033,#FF0000";
+    color=color.split(",");
+    document.getElementById("msg").style.color=color[parseInt(Math.random()*color.length)];
+}
+var flagtime; 
+flagtime = setInterval("blink()",200);
+function replace() {
+    document.getElementById('msg').innerHTML = 'Connect successfully!';
+    window.clearTimeout(flagtime);
+}
+setTimeout("replace()",10000);
+var flag = 1;
+var start = document.getElementById('start');
+var pause = document.getElementById('pause');
+start.onclick = function() {
+    if (flag == 1) {
         result_player1.innerHTML = "<span></span>";
-        flag[0] = 0;
+        fun();
+        flag = 0;
     }
-    fun();
-    flag2 = 1;
 }
-start[1].onclick = function() {
-    j = 1;
-    if (flag[1] == 1) {
-        result_player2.innerHTML = "<span></span>";
-        flag[1] = 0;
+pause.onclick = function() {
+    if (flag == 0) {
+        fun2();
+        flag = 1; 
     }
-    fun();
-    flag3 = 1;
 }
-stop1[0].onclick = function() {
-    if (flag3 == 1) {
-        alert("请在你所在的玩家位置按停止键,本轮结果不算数");
-    }
-    fun2();
-    result_player1.innerHTML = "<span>" + result + "</span>";
-    flag[0] = 1;
-    flag2 = 0;
-}
-stop1[1].onclick = function() {
-    if (flag2 == 1) {
-        alert("请在你所在的玩家位置按停止键，本轮结果不算数");
-    }
-    fun2();
-    result_player2.innerHTML = "<span>" + result + "</span>";
-    flag[1] = 1;
-    flag3 = 0;
-
-}
-var flagtime;
 var oldinner;
 var num;
 num = new Array(6);
 
 function init() {
-    pic1[j].src = "pic/dice_pic1.jpg";
-    pic2[j].src = "pic/dice_pic2.jpg";
-    pic3[j].src = "pic/dice_pic3.jpg";
-    pic4[j].src = "pic/dice_pic4.jpg";
-    pic5[j].src = "pic/dice_pic5.jpg";
-    pic6[j].src = "pic/dice_pic6.jpg";
+    pic1.src = "pic/dice_pic1.jpg";
+    pic2.src = "pic/dice_pic2.jpg";
+    pic3.src = "pic/dice_pic3.jpg";
+    pic4.src = "pic/dice_pic4.jpg";
+    pic5.src = "pic/dice_pic5.jpg";
+    pic6.src = "pic/dice_pic6.jpg";
 }
 
 function fun() {
@@ -79,14 +60,13 @@ function fun() {
         arr[i] = Math.floor(Math.random() * 6 + 1);
         num[arr[i] - 1]++;
     }
-    pic1[j].src = "pic/dice_pic" + arr[0] + ".jpg";
-    pic2[j].src = "pic/dice_pic" + arr[1] + ".jpg";
-    pic3[j].src = "pic/dice_pic" + arr[2] + ".jpg";
-    pic4[j].src = "pic/dice_pic" + arr[3] + ".jpg";
-    pic5[j].src = "pic/dice_pic" + arr[4] + ".jpg";
-    pic6[j].src = "pic/dice_pic" + arr[5] + ".jpg";
-    flagtime = setTimeout(fun, 100,);
-
+    pic1.src = "pic/dice_pic" + arr[0] + ".jpg";
+    pic2.src = "pic/dice_pic" + arr[1] + ".jpg";
+    pic3.src = "pic/dice_pic" + arr[2] + ".jpg";
+    pic4.src = "pic/dice_pic" + arr[3] + ".jpg";
+    pic5.src = "pic/dice_pic" + arr[4] + ".jpg";
+    pic6.src = "pic/dice_pic" + arr[5] + ".jpg";
+    flagtime = setTimeout("fun()", 100);
 }
 
 function fun2() {
@@ -94,35 +74,35 @@ function fun2() {
     window.clearTimeout(flagtime);
     if (num[3] >= 4) {
         if (num[3] == 6)
-            result = "状元-红六勃";
+            result_player1.innerHTML = "<span>状元-红六勃</span>";
         else if (num[3] == 5)
-            result = "状元-五红";
+            result_player1.innerHTML = "<span>状元-五红</span>";
         else if (num[0] == 2)
-            result = "状元-插金花";
+            result_player1.innerHTML = "<span>状元-插金花</span>";
         else
-            result = "状元-四红";
+            result_player1.innerHTML = "<span>状元-四红</span>";
     } else if (num[0] == 6)
-        result = "状元-遍地锦";
+        result_player1.innerHTML = "<span>状元-遍地锦</span>";
     else if (num[1] == 6 || num[2] == 6 || num[4] == 6 || num[5] == 6)
-        result = "状元-黒六勃";
+        result_player1.innerHTML = "<span>状元-黒六勃</span>";
     else if (num[0] == 5 || num[1] == 5 || num[2] == 5 || num[4] == 5 || num[5] == 5)
-        result = "状元-五子登科";
+        result_player1.innerHTML = "<span>状元-五子登科</span>";   
     else {
         for (var i = 0; i < 6; i++) {
             if (num[i] == 4)
                 sijin = 1;
         }
         if (sijin == 1)
-            result = "进士-四进";
+            result_player1.innerHTML = "<span>进士-四进</span>";
         else if (num[0] == 1 && num[1] == 1 && num[2] == 1 && num[3] == 1 && num[4] == 1 && num[5] == 1)
-            result = "榜眼-对堂";
+            result_player1.innerHTML = "<span>榜眼-对堂</span>";
         else if (num[3] == 3)
-            result = "探花-三红";
+            result_player1.innerHTML = "<span>探花-三红</span>";
         else if (num[3] == 2)
-            result = "举人-二举";
+            result_player1.innerHTML = "<span>举人-二举</span>";
         else if (num[3] == 1)
-            result = "秀才-一秀";
+            result_player1.innerHTML = "<span>秀才-一秀</span>";
         else
-            result = "哎呀，再博一次吧";
+            result_player1.innerHTML = "<span>哎呀，再博一次吧</span>";
     }
 }
